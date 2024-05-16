4b56ed7186853e58e1b3786327585b5c90fc201f
ca-osvaldas <osvaldas.karpas@codeacademylt.onmicrosoft.com>
2024-05-16 20:09:32 +0300

    Fixed linting error

--- app_test.js
+++ app_test.js
@@ -4,8 +4,10 @@ const express = require("express");
 const app = express();
 app.get("/", (req, res) => res.send("Hello, World!"));
 
-test("GET / responds with Hello, World!", async () => {
-  const response = await request(app).get("/");
-  expect(response.text).toBe("Hello, World!");
-  expect(response.statusCode).toBe(200);
+describe("GET /", () => {
+  test("responds with Hello, World!", async () => {
+    const response = await request(app).get("/");
+    expect(response.text).toBe("Hello, World!");
+    expect(response.statusCode).toBe(200);
+  });
 });
